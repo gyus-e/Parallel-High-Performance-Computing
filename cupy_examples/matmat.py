dc4de01d86a1106eec389162ad05881b403cc0e3
giuseppeamato <giuseppe.mta97@gmail.com>
2026-06-19 22:45:56 +0200

    added swizzled version

--- cupy_examples/matmat.py
+++ cupy_examples/matmat.py
@@ -28,6 +28,30 @@ def matmat_kernel(
     ct.store(c, index=(ii, jj), tile=acc)
 
 
+@ct.kernel
+def matmat_swizzled_kernel(
+    a: cp.ndarray,
+    b: cp.ndarray,
+    c: cp.ndarray,
+    tn: ct.Constant[int],
+    tk: ct.Constant[int],
+    tm: ct.Constant[int],
+):
+    ii, jj = utils.swizzle_2d(m=c.shape[0], n=c.shape[1], tm=tm, tn=tn, group_size_m=4)
+
+    num_tiles = ct.num_tiles(a, axis=1, shape=(tn, tk))
+    # num_tiles = ct.num_tiles(b, axis=0, shape=(tk, tm))
+
+    acc = ct.full((tn, tm), 0.0, dtype=a.dtype)
+
+    for kk in range(num_tiles):
+        a_tile = ct.load(a, index=(ii, kk), shape=(tn, tk))
+        b_tile = ct.load(b, index=(kk, jj), shape=(tk, tm))
+        acc = ct.mma(a_tile, b_tile, acc)
+
+    ct.store(c, index=(ii, jj), tile=acc)
+
+
 def test_matmat(
     a: cp.ndarray,
     b: cp.ndarray,
@@ -57,26 +81,61 @@ def test_matmat(
     utils.assert_close(c_np, expected, "matmat_kernel")
 
 
-def main():
-    n = 2**10
-    k = 2**8
-    m = 2**9
+def test_matmat_swizzled(
+    a: cp.ndarray,
+    b: cp.ndarray,
+    c: cp.ndarray,
+    tn: ct.Constant[int],
+    tk: ct.Constant[int],
+    tm: ct.Constant[int],
+    grid: utils.Dim3,
+    stream: cp.cuda.Stream = cp.cuda.get_current_stream(),
+):
+    assert a.dtype == b.dtype == c.dtype
+    assert a.shape[1] == b.shape[0]
+    assert c.shape[0] == a.shape[0] and c.shape[1] == b.shape[1]
 
+    ct.launch(
+        stream,
+        grid,
+        matmat_swizzled_kernel,
+        (a, b, c, tn, tk, tm),
+    )
+
+    a_np = cp.asnumpy(a)
+    b_np = cp.asnumpy(b)
+    c_np = cp.asnumpy(c)
+    expected = a_np @ b_np
+
+    utils.assert_close(c_np, expected, "matmat_swizzled_kernel")
+
+
+def main():
     tn = 256
     tk = 64
     tm = 128
 
+    n = 2**10
+    k = 2**8
+    m = 2**9
+
+    grid = (int(ct.cdiv(n, tn)), int(ct.cdiv(m, tm)), 1)
+    stream = cp.cuda.get_current_stream()
+
     rng: cp.random._generator_api = cp.random.default_rng()
+
     a = rng.random((n, k))
     b = rng.random((k, m))
     c = cp.zeros((n, m), dtype=a.dtype)
 
-    stream = cp.cuda.get_current_stream()
-    grid = (int(ct.cdiv(n, tn)), int(ct.cdiv(m, tm)), 1)
-
+    # Test
     test_matmat(a, b, c, tn, tk, tm, grid, stream)
     print("✓ matmat_kernel passed!")
 
+    test_matmat_swizzled(a, b, c, tn, tk, tm, grid, stream)
+    print("✓ matmat_swizzled_kernel passed!")
+
+    # Benchmark
     time = utils.bench(
         matmat_kernel,
         (a, b, c, tn, tk, tm),
@@ -87,7 +146,15 @@ def main():
     )
     print(f"matmat_kernel: {time:.3e} sec/iter")
 
-
+    time = utils.bench(
+        matmat_swizzled_kernel,
+        (a, b, c, tn, tk, tm),
+        grid=grid,
+        stream=stream,
+        warmup=10,
+        iters=100,
+    )
+    print(f"matmat_swizzled_kernel: {time:.3e} sec/iter")
 
 if __name__ == "__main__":
     main()
--- cupy_examples/utils.py
+++ cupy_examples/utils.py
@@ -2,9 +2,10 @@ import numpy as np
 import cupy as cp
 import cuda.tile as ct
 import time
-from typing import Any, Sequence, TypeAlias
+from typing import TypeAlias
 
 Dim3: TypeAlias = tuple[int] | tuple[int, int] | tuple[int, int, int]
+
 
 def assert_close(actual, desired, name: str, rtol=1e-5, atol=0.0):
     np.testing.assert_allclose(
@@ -13,7 +14,7 @@ def assert_close(actual, desired, name: str, rtol=1e-5, atol=0.0):
     print(f"{name}: OK")
 
 
-def bench(kernel, args, grid: Dim3, stream, warmup=5, iters=30):    
+def bench(kernel, args, grid: Dim3, stream, warmup=5, iters=30):
     for _ in range(warmup):
         ct.launch(stream, grid, kernel, args)
     stream.synchronize()
@@ -24,3 +25,22 @@ def bench(kernel, args, grid: Dim3, stream, warmup=5, iters=30):
     stream.synchronize()
     t1 = time.perf_counter()
     return (t1 - t0) / iters
+
+
+def swizzle_2d(n: int, m: int, tn: int, tm: int, group_size_m: int) -> tuple[int, int]:
+    pid = ct.bid(0)
+    num_tiles_n = ct.cdiv(n, tn)
+    num_tiles_m = ct.cdiv(m, tm)
+
+    num_tiles_in_group = group_size_m * num_tiles_n
+    group_id = pid // num_tiles_in_group
+    first_tile_m = group_id * group_size_m
+
+    diff = num_tiles_m - first_tile_m
+    group_size_m = min(diff, group_size_m)
+    pid_in_group = pid % num_tiles_in_group
+
+    bidx = first_tile_m + (pid_in_group % group_size_m)
+    bidy = pid_in_group // group_size_m
+
+    return bidx, bidy
